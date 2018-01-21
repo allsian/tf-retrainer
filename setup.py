75511ff1959472f3974067bbf0e3f6fd118b5f3a
Justin Quick <justquick@gmail.com>
2018-01-21 16:18:18 -0500

    added setup.py and BSD license

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='tf_retrainer',
+      version='1.0',
+      description='transfer learning in tensorflow',
+      long_description=open('README.md').read(),
+      author='Chris Messier',
+      license='BSD 3-Clause',
+      author_email='messiercr@gmail.com',
+      url='https://github.com/messiest/tf-retrainer',
+      packages=['tf_retrainer'],
+      classifiers=['Development Status :: 1 - Planning',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Scientific/Engineering :: Artificial Intelligence'],
+      )
