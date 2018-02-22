458e85f0d0881da840d50b83067ec7212d10edd3
Chris Messier <messiercr@gmail.com>
2018-02-22 18:40:35 -0500

    updated the demo file

--- demo.sh
+++ demo.sh
@@ -2,10 +2,14 @@
 
 cd tf_retrainer
 
+if [ ! -f cifar-extender/cifar_extender/data ]; then
+    bash cifar-extender/demo.sh
+fi
+
 echo "retraining the model..."
 # retrain the last layer of the inception net on the training data
 python3 image_retraining/retrain.py \
-    --image_dir cifar-extender/data/images/\
+    --image_dir cifar-extender/cifar_extender/data/images/\
     --output_graph models/stackline.pb\
     --intermediate_output_graphs_dir logs/intermediate_graph/\
     --intermediate_store_frequency 500\
